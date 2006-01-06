81c05eec0468edcdb6f5364fc1d030f836534222
ivop <ivop@393ad9ac-c348-0410-aaaa-eed0ef1b8d0c>
2006-01-06 18:35:41 +0000

    detect inttypes.h or stdint.h if both are unavailable, define (u)int*_t types ourselves

--- trunk/fpm/genlut.c
+++ trunk/fpm/genlut.c
@@ -14,7 +14,22 @@
 
 #include <math.h>
 #include <stdio.h>
+#ifdef HAVE_INTTYPES_H
 #include <inttypes.h>
+#else
+#ifdef HAVE_STDINT_H
+#include <stdint.h>
+#else
+typedef unsigned long long uint64_t;
+typedef          long long  int64_t;
+typedef unsigned long      uint32_t;
+typedef          long       int32_t;
+typedef unsigned short     uint16_t;
+typedef          short      int16_t;
+typedef unsigned char      uint8_t;
+typedef          char       int8_t;
+#endif
+#endif
 
 /* ------------------------------------------------------------------------- */
 
--- trunk/fpm/include/fixedpointmath.h
+++ trunk/fpm/include/fixedpointmath.h
@@ -24,7 +24,23 @@
 
 /* ------------------------------------------------------------------------- */
 
+#ifdef HAVE_INTTYPES_H
 #include <inttypes.h>
+#else
+#ifdef HAVE_STDINT_H
+#include <stdint.h>
+#else
+typedef unsigned long long uint64_t;
+typedef          long long  int64_t;
+typedef unsigned long      uint32_t;
+typedef          long       int32_t;
+typedef unsigned short     uint16_t;
+typedef          short      int16_t;
+typedef unsigned char      uint8_t;
+typedef          char       int8_t;
+#endif
+#endif
+
 #include "fixedpointmathcode.h"
 
 /* ------------------------------------------------------------------------- */
