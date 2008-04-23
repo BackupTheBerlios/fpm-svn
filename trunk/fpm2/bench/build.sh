624068c405e32f21e695827918a2b33e5f43c256
ivop <ivop@393ad9ac-c348-0410-aaaa-eed0ef1b8d0c>
2008-04-23 09:11:19 +0000

    allow more extra flags for various builds

--- trunk/fpm2/bench/build.sh
+++ trunk/fpm2/bench/build.sh
@@ -9,12 +9,14 @@ toupper() {
 }
 
 build_all() {
+    pf=$1
+    shift 1
     for i in float double \
              fp8p8 fp24p8 fp16p16 fp8p24 \
              ufp8p8 ufp24p8 ufp16p16 ufp8p24
     do
         j=`toupper $i`
-        $CC -o bench-$i$1 bench.c -I`pwd`/../include -lm -DDO$j $2
+        $CC -o bench-$i$pf bench.c -I`pwd`/../include -lm -DDO$j $@
     done
 }
 
